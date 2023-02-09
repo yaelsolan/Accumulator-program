6365a2e093172ddcd2c0ba835a3f75bc3103aba1
Yael Solan <yaelsolan5@gmail.com>
2023-02-09 16:36:01 +0200

    another way to determine program name

--- myprogram.py
+++ myprogram.py
@@ -1,4 +1,3 @@
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--foo', help='foo help')
-args=parser.parse_args()
+parser = argparse.ArgumentParser(prog='myprogram')
+parser.print_help()
