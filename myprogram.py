61dfcf920b1315b41a30956bf20fa5e82a3a0249
Yael Solan <yaelsolan5@gmail.com>
2023-02-09 17:42:51 +0200

    add description

--- myprogram.py
+++ myprogram.py
@@ -1,4 +1,4 @@
 import argparse
-parser = argparse.ArgumentParser(prog='myprogram')
+parser = argparse.ArgumentParser(prog='myprogram', description='A foo that bars')
 parser.add_argument('--foo', help='foo of the %(prog)s program')
 parser.print_help()
