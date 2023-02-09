dfee7635ac11976a02fbb8a755b8a27d0e424d1c
Yael Solan <yaelsolan5@gmail.com>
2023-02-09 17:34:45 +0200

    override default message with the usage= keyword argument

--- prog.py
+++ prog.py
@@ -10,7 +10,7 @@ parser.add_argument('--sum', dest='accumulate', action='store_const',
 args = parser.parse_args()
 print(args.accumulate(args.integers))
 
-parser = argparse.ArgumentParser(prog='PROG')
+parser = argparse.ArgumentParser(prog='PROG', usage='%(prog)s [options]')
 parser.add_argument('--foo', nargs='?', help='foo help')
 parser.add_argument('bar', nargs='+', help='bar help')
 parser.print_help()
