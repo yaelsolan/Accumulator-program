4f11c9f1b2cecad0a72a3a8b5e5ab4ff013003ce
Yael Solan <yaelsolan5@gmail.com>
2023-02-09 17:46:27 +0200

    add epilog

--- myprogram.py
+++ myprogram.py
@@ -1,4 +1,7 @@
 import argparse
-parser = argparse.ArgumentParser(prog='myprogram', description='A foo that bars')
+parser = argparse.ArgumentParser(
+	prog='myprogram',
+	description='A foo that bars',
+	epilog="And that's how you'd foo a bar")
 parser.add_argument('--foo', help='foo of the %(prog)s program')
 parser.print_help()
