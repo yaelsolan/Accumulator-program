670774eb91e2577036c6fdb60483f2363a3bc28e
Yael Solan <yaelsolan5@gmail.com>
2023-02-11 01:23:41 +0200

    fromfile_prefix_chars

--- prog.py
+++ prog.py
@@ -1,9 +1,10 @@
 import argparse, textwrap
 
-parser = argparse.ArgumentParser(prog='PROG', prefix_chars='-+')
-parser.add_argument('+f')
-parser.add_argument('++bar')
-parser.parse_args('+f X ++bar Y'.split())
+with open ('args.txt', 'w') as fp:
+	fp.write('-f\nbar')
+parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+parser.add_argument('-f')
+parser.parse_args(['-f','foo', '@args.txt'])
 parser.add_argument('integers', type=int, nargs='+',
                     help='an integer for the accumulator')
 parser.add_argument('--sum', dest='accumulate', action='store_const',
