db31b0c4a812ed8d18899d9a50f6f5dabc85d319
Yael Solan <yaelsolan5@gmail.com>
2023-02-11 17:37:25 +0200

    add resolve to conflict_handler

--- prog.py
+++ prog.py
@@ -1,6 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser(prog='PROG')
+parser = argparse.ArgumentParser(prog='PROG', conflict_handler='resolve')
 parser.add_argument('-f', '--foo', help='old foo help')
 parser.add_argument('--foo', help='new foo help')
 
