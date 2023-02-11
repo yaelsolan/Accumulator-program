46ecd8b0404a8360dd5f73871537b39ed0f28b4d
Yael Solan <yaelsolan5@gmail.com>
2023-02-11 19:29:50 +0200

    add class FooAction

--- myprogram.py
+++ myprogram.py
@@ -18,3 +18,16 @@ bar_parser = argparse.ArgumentParser(parents=[parent_parser])
 bar_parser.add_argument('--bar')
 bar_parser.parse_args(['--bar', 'YYY'])
 
+class FooAction(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError("nargs not allowed")
+        super().__init__(option_strings, dest, **kwargs)
+    def __call__(self, parser, namespace, values, option_string=None):
+        print('%r %r %r' % (namespace, values, option_string))
+        setattr(namespace, self.dest, values)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--foo', action=FooAction)
+parser.add_argument('bar', action=FooAction)
+args = parser.parse_args('1 --foo 2'.split())
--- prog.py
+++ prog.py
@@ -18,3 +18,5 @@ parser = argparse.ArgumentParser(prog='PROG', usage='%(prog)s [options]')
 parser.add_argument('--foo', nargs='?', help='foo help')
 parser.add_argument('bar', nargs='+', help='bar help')
 parser.print_help()
+
+
