ba15d60e5a0a94b67f9dd80ddb553be24acae02a
Yael Solan <yaelsolan5@gmail.com>
2023-02-11 17:50:48 +0200

    prexic chars option

--- myprogram.py
+++ myprogram.py
@@ -3,7 +3,7 @@ parser = argparse.ArgumentParser(
 	prog='myprogram',
 	description='A foo that bars',
 	epilog="And that's how you'd foo a bar",
-	add_help=False)
+	prefix_chars='+/')
 parser.add_argument('--foo', help='foo of the %(prog)s program')
 parser.print_help()
 
