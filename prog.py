ca0539127806618ce9c497daec9841d2f933976e
Yael Solan <yaelsolan5@gmail.com>
2023-02-12 02:52:39 +0200

    sub-command functions

--- prog.py
+++ prog.py
@@ -17,6 +17,13 @@ print(args.accumulate(args.integers))
 parser = argparse.ArgumentParser(prog='PROG', usage='%(prog)s [options]')
 parser.add_argument('--foo', nargs='?', help='foo help')
 parser.add_argument('bar', nargs='+', help='bar help')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--foo', nargs='?', const='c', default='d')
+parser.add_argument('bar', nargs='?', default='d')
+parser.parse_args(['XX', '--foo', 'YY'])
+parser.parse_args(['XX', '--foo'])
+parser.parse_args([])
 parser.print_help()
 
 
